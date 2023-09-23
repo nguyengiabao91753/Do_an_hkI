7a2891c244a77d815f8bbf0074fd62b7e213ac90
nguyengiabao91753 <140630293+nguyengiabao91753@users.noreply.github.com>
2023-09-23 14:11:35 +0700

    e vừa chỉnh lại bố cục

---
+++ database/migrations/2023_09_22_171823_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->integer('CustomerID')->primary();
+            $table->string('FirstName');
+            $table->string('LastName');
+            $table->string('Email');
+            $table->string('Phone');
+            $table->string('Address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ resources/views/admin/layout/app.blade.php
@@ -0,0 +1,11 @@
+@include('admin.layout.header')
+
+<body>
+    @include('admin.layout.navigation')
+
+
+   
+    @include('admin.layout.navigation')
+</body>
+
+</html>
